780d7e4e3aa769453bab6320a9184ac8afa35e19
sungyeah <98869025+sungyeah@users.noreply.github.com>
2024-03-18 13:52:13 +0900

    Initial Commit

---
+++ BankDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankDiary
+//
+//  Created by GoldenPlanet on 3/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
