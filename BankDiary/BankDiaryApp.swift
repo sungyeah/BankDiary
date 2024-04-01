4c40aa9777f460a31e149c500a088c65d539e52a
sungyeah <98869025+sungyeah@users.noreply.github.com>
2024-04-01 18:08:13 +0900

    Initial Commit

---
+++ BankDiary/BankDiaryApp.swift
@@ -0,0 +1,20 @@
+//
+//  BankDiaryApp.swift
+//  BankDiary
+//
+//  Created by GoldenPlanet on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct BankDiaryApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
