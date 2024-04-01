cb61bd28b46b84eb8564769d27fb9fbcdad853f4
sungyeah <98869025+sungyeah@users.noreply.github.com>
2024-04-01 18:16:36 +0900

    aaaaa

--- BankDiary/ContentView.swift
+++ BankDiary/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
             List {
                 ForEach(items) { item in
                     NavigationLink {
-                        Text("Item atㅁㅁㅁ \(item.timestamp!, formatter: itemFormatter)")
+                        Text("Item ataa \(item.timestamp!, formatter: itemFormatter)")
                     } label: {
                         Text(item.timestamp!, formatter: itemFormatter)
                     }
