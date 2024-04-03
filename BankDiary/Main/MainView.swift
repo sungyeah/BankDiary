90926f9476c19636a01f46ef071ad56b010b3857
sungyeah <98869025+sungyeah@users.noreply.github.com>
2024-04-04 00:08:51 +0900

    은행 아이콘 재업로드

--- BankDiary/Main/MainView.swift
+++ BankDiary/Main/MainView.swift
@@ -20,7 +20,8 @@ struct MainView: View {
         Asset(bank: "우리", accountName: "WON 통장", amount: 450000, image: "woori"),
         Asset(bank: "농협", accountName: "NH주거리우대통장", amount: 638530, image: "nh"),
         Asset(bank: "국민", accountName: "KB나라사랑우대통장", amount: 33500, image: "kb"),
-        Asset(bank: "토스뱅크", accountName: "토스뱅크통장", amount: 12500, image: "toss")
+        Asset(bank: "토스뱅크", accountName: "토스뱅크통장", amount: 12500, image: "toss"),
+        Asset(bank: "카카오뱅크", accountName: "카카오뱅크통장", amount: 1650, image: "kakao")
     ]
     
     
@@ -41,7 +42,7 @@ struct MainView: View {
                 Text("Bank Diary").font(.largeTitle).foregroundColor(.point).bold()
                 Spacer()
             }
-            .padding(.horizontal,13).padding(.bottom, 1)
+            .padding(.horizontal, 15).padding(.vertical, 10.0)
         }.background(Color.background)
     }
     
